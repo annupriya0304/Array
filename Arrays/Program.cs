fedb9b5bb6246106982980559d304324922f488b
annpriya <annupriya0304@gmail.com>
2018-12-03 11:31:54 +0530

    mn max program changes

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,15 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            int testcase = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < testcase; i++)
+            {
+                int i = Convert.ToInt32(Console.ReadLine());
+                int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+                Array.Sort(arr);
+                Console.WriteLine(arr[0] + " " + arr[arr.Length - 1]);
+
+            }
         }
     }
 }
